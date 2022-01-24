fd3361020e93994dc22f198dc77a887da9d537bd
Ibukunoluwa985 <promisenap2001@gmail.com>
2022-01-24 21:48:24 +0100

    initial commit

---
+++ account/models.py
@@ -0,0 +1,90 @@
+from django.db import models
+from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser)
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_tutoruser(self, email, password):
+        """
+        Creates and saves a tutor user with the given email and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.tutor = True
+        user.save(using=self._db)
+        return user
+
+    def create_adminuser(self, email, password):
+        """
+        Creates and saves a adminuser with the given email and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.tutor = True
+        user.admin = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser):
+    objects = UserManager()
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    is_active = models.BooleanField(default=True)
+    tutor = models.BooleanField(default=False) # a tutor user; non super-user
+    admin = models.BooleanField(default=False) # a superuser
+
+    # notice the absence of a "Password field", that is built in.
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [] # Email & Password are required by default.
+
+    def get_full_name(self):
+        # The user is identified by their email address
+        return self.email
+
+    def get_short_name(self):
+        # The user is identified by their email address
+        return self.email
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_tutor(self):
+        "Is the user a member of tutors?"
+        return self.tutor
+
+    @property
+    def is_admin(self):
+        "Is the user a admin member?"
+        return self.admin
