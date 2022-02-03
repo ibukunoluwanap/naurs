0beff2a7d72e1ac29485cf3d20258ffbc776e126
Ibukunoluwa985 <promisenap2001@gmail.com>
2022-02-03 18:03:22 +0100

    added static

--- naurs/urls.py
+++ naurs/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('dev_admin/', admin.site.urls),
     path('', include('home.urls')),
     path('account/', include('account.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
