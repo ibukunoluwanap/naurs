374ac7cea66f7cc7b37c1dad189ac5854a053dea
Ibukunoluwa985 <promisenap2001@gmail.com>
2022-02-04 12:05:48 +0100

    created login and forgot password page

--- account/views.py
+++ account/views.py
@@ -6,3 +6,15 @@ class Register(View):
     template_name = "account/register.html"
     def get(self, request):
         return render(request, self.template_name)
+
+# login view
+class Login(View):
+    template_name = "account/login.html"
+    def get(self, request):
+        return render(request, self.template_name)
+
+# forgot view
+class ForgetPassword(View):
+    template_name = "account/forget_password.html"
+    def get(self, request):
+        return render(request, self.template_name)
