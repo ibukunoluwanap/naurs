396986a46e486d2732cbad0662760cf83882feb9
Ibukunoluwa985 <promisenap2001@gmail.com>
2022-02-05 15:41:12 +0100

    completed forget password

--- account/urls.py
+++ account/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('logout/', views.Logout.as_view(), name='logout_page'),
 
     # forgot password
-    path('forget_password/', auth_views.PasswordResetView.as_view(template_name="account/forgot_password.html", html_email_template_name="account/forgot_password_email.html"), name='password_reset'),
+    path('forget_password/', auth_views.PasswordResetView.as_view(template_name="account/forgot_password.html", html_email_template_name="account/forgot_password_email.html", subject_template_name="account/forget_password_subject.txt"),  name='password_reset'),
     path('forget_password_done/', views.PasswordResetDone.as_view(), name='password_reset_done'),
     path('forget_password_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="account/forgot_password_confirm.html"), name='password_reset_confirm'),
     path('forget_password_complete/', views.PasswordResetComplete.as_view(), name="password_reset_complete"),
