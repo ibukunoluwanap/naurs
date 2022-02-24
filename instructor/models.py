daf43a9ecbf8043afe105b74dda3dc3d53178ffc
Ibukunoluwa985 <promisenap2001@gmail.com>
2022-02-24 16:34:09 +0100

    made instructor model program many to many

--- instructor/models.py
+++ instructor/models.py
@@ -9,7 +9,7 @@ User = get_user_model()
 # instructor modal
 class InstructorModel(models.Model):
     user = models.ForeignKey(User, verbose_name="user", on_delete=models.CASCADE)
-    program = models.ForeignKey(ProgramModel, verbose_name="program", on_delete=models.CASCADE)
+    program = models.ManyToManyField(ProgramModel, verbose_name="program")
     avatar = models.FileField("avatar", upload_to="avatar/", max_length=100, blank=False, null=False)
     role = models.CharField("role", max_length=100, blank=False, null=False)
     about = HTMLField()
