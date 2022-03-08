4af1ad9aee664160f2753e5dc20bb39c06d449ae
Ibukunoluwa985 <promisenap2001@gmail.com>
2022-03-08 16:27:28 +0100

    created ProgramPayment model on programs

--- program/models.py
+++ program/models.py
@@ -1,4 +1,6 @@
+from email.policy import default
 from django.db import models
+from django.forms import BooleanField
 from tinymce.models import HTMLField
 
 # program category
@@ -15,6 +17,7 @@ class ProgramModel(models.Model):
     category = models.CharField("category", choices=PROGRAM_CATEGORY, default=PROGRAM_CATEGORY[0], max_length=100, blank=False, null=False)
     title = models.CharField("title", max_length=100, blank=False, null=False)
     price = models.PositiveIntegerField()
+    total_space = models.PositiveIntegerField()
     info = HTMLField()
     created_on = models.DateTimeField("created on", auto_now_add=True)
 
@@ -53,3 +56,21 @@ class ProgramEnquiryModel(models.Model):
 
     def __str__(self):
         return f"{self.program.title} enquiry from {self.name}"
+
+class ProgramPayment(models.Model):
+    program = models.ForeignKey(ProgramModel, verbose_name="program", on_delete=models.CASCADE)
+    first_name = models.CharField("first name", max_length=100, blank=False, null=False)
+    last_name = models.CharField("last name", max_length=100, blank=False, null=False)
+    email = models.EmailField(max_length=255, blank=False, null=False)
+    completed = models.BooleanField(default=False)
+    created_on = models.DateTimeField("created on", auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'ProgramPayment'
+        verbose_name_plural = 'ProgramPayments'
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return f"Payment by {self.get_full_name()}"
