c6ec302e603deda36e3c59e59a271c63929bc187
Ibukunoluwa985 <promisenap2001@gmail.com>
2022-03-21 19:40:21 +0100

    updated program

--- program/admin.py
+++ program/admin.py
@@ -1,14 +1,7 @@
 from django.contrib import admin
 from .models import ProgramModel, ProgramBenefitModel, ProgramEnquiryModel, ProgramPaymentModel
 
-class ProgramBenefitInline(admin.TabularInline):
-    model = ProgramBenefitModel
-
-class ProgramEnquiryInline(admin.StackedInline):
-    model = ProgramEnquiryModel
-
-class ProgramAdmin(admin.ModelAdmin):
-   inlines = [ProgramBenefitInline, ProgramEnquiryInline]
-
-admin.site.register(ProgramModel, ProgramAdmin)
+admin.site.register(ProgramModel)
+admin.site.register(ProgramEnquiryModel)
+admin.site.register(ProgramBenefitModel)
 admin.site.register(ProgramPaymentModel)
