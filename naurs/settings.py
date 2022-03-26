df7e721c44f3ae6e30cf08ac371e9fb18fdf9758
Ibukunoluwa985 <promisenap2001@gmail.com>
2022-03-26 16:52:46 +0100

    moved SECRET_KEY to .env

--- naurs/settings.py
+++ naurs/settings.py
@@ -1,10 +1,11 @@
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+1kr3*+ggrjij0k#rbjrp+_7)ig^8oc=k5x3ubl1&@p-1^6&=r'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
