b7d3e5074f856ca127d5cd57fe1fd65b3268ed10
Ibukunoluwa985 <promisenap2001@gmail.com>
2022-04-10 17:45:05 +0100

    update student views

--- student/forms.py
+++ student/forms.py
@@ -1,12 +1,14 @@
 from django import forms
+from instructor.models import InstructorModel
 from program.forms import CustomMultipleChoiceField
 from program.models import ProgramModel
 from student.models import StudentModel
 
 # student form
 class StudentForm(forms.ModelForm):
+    instructor = CustomMultipleChoiceField(required=True, widget=forms.CheckboxSelectMultiple, queryset=InstructorModel.objects.all())
     program = CustomMultipleChoiceField(required=True, widget=forms.CheckboxSelectMultiple, queryset=ProgramModel.objects.all())
 
     class Meta:
         model = StudentModel
-        exclude = ['created_on']
+        exclude = ['user', 'created_on']
--- student/models.py
+++ student/models.py
@@ -9,7 +9,7 @@ User = get_user_model()
 # student modal
 class StudentModel(models.Model):
     user = models.OneToOneField(User, verbose_name="user", on_delete=models.CASCADE)
-    instructor = models.ForeignKey(InstructorModel, verbose_name="instructor", null=True, on_delete=models.SET_NULL)
+    instructor = models.ManyToManyField(InstructorModel, verbose_name="instructor")
     program = models.ManyToManyField(ProgramModel, verbose_name="class")
     created_on = models.DateTimeField("created on", auto_now_add=True)
 
