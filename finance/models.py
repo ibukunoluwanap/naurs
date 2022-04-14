6a4cdc99f2b82bf6fd72912118531f3f8fa076a4
Ibukunoluwa985 <promisenap2001@gmail.com>
2022-04-14 19:22:56 +0100

    create wallet model

--- finance/models.py
+++ finance/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+# setting User model
+User = get_user_model()
+
+# wallet modal
+class WalletModel(models.Model):
+    user = models.ForeignKey(User, verbose_name="user", on_delete=models.CASCADE)
+    balance = models.PositiveIntegerField()
+    updated_on = models.DateTimeField("updated on", auto_now=True)
+
+    class Meta:
+        verbose_name = 'Wallet'
+        verbose_name_plural = 'Wallet'
+
+    def __str__(self):
+        return f"{self.title}"
