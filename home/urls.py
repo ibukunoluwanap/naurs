f56adf67fb1c5697b87788709fc0f026235e9408
Ibukunoluwa985 <promisenap2001@gmail.com>
2022-04-15 09:40:02 +0100

    update calendar

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home_page'),
-    path('calendar/', views.CalendarView.as_view(), name='program_calendar'),
+    path('calendar/', views.CalendarView.as_view(), name='calendar'),
 ]
