0eca2e951ea0ce99d9d8d465a38de23f26716557
Ibukunoluwa985 <promisenap2001@gmail.com>
2022-04-15 21:43:59 +0100

    created billing address model

--- finance/models.py
+++ finance/models.py
@@ -16,3 +16,20 @@ class WalletModel(models.Model):
 
     def __str__(self):
         return f"{self.user}"
+
+# billing address modal
+class BillingAddressModel(models.Model):
+    user = models.OneToOneField(User, verbose_name="user", on_delete=models.CASCADE)
+    street_address = models.CharField("street address", max_length=250, blank=False, null=False)
+    city_town = models.CharField("city/town", max_length=100, blank=False, null=False)
+    country = models.CharField("country", max_length=250, blank=False, null=False)
+    zip_code = models.CharField("zip code", max_length=50, blank=False, null=False)
+    updated_on = models.DateTimeField("updated on", auto_now=True)
+
+    class Meta:
+        verbose_name = 'BillingAddress'
+        verbose_name_plural = 'BillingAddresses'
+
+    def __str__(self):
+        return f"{self.user}"
+
