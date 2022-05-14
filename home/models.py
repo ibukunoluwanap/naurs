6da5afa7f8a857dbea84346f51a935753d9010f8
Ibukunoluwa Naphtali <promisenap2001@gmail.com>
2022-05-14 14:35:43 +0200

    create studio model

--- home/models.py
+++ home/models.py
@@ -38,3 +38,30 @@ class CalendarModel(models.Model):
 
     def __str__(self):
         return f"{self.program} calendar"
+
+# studio modal
+class StudioModel(models.Model):
+    name = models.CharField(verbose_name="name", max_length=255)
+    is_active = models.BooleanField(default=False)
+    created_on = models.DateTimeField("created on", auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Studio'
+        verbose_name_plural = 'Studios'
+
+    def __str__(self):
+        return f"{self.name}"
+
+# studio user modal
+class StudioUserModel(models.Model):
+    studio = models.ForeignKey(StudioModel, verbose_name="studio", on_delete=models.CASCADE)
+    full_name = models.CharField(verbose_name="Full name", max_length=200)
+    email = models.EmailField(verbose_name='email address', max_length=255)
+    created_on = models.DateTimeField("created on", auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Studio'
+        verbose_name_plural = 'Studios'
+
+    def __str__(self):
+        return f"{self.full_name} at {self.studio}"
